368c265ceb9fa12e585bf9bffc05fe9cfb7ff222
Gardient <gradient93@gmail.com>
2017-12-19 23:02:39 +0200

    xkcd calendar fact generator

---
+++ content/xkcd-calendar-facts.md
@@ -0,0 +1,10 @@
+---
+layout: xkcd-calendar-facts
+title: "Xkcd Calendar Facts"
+date: 2017-12-19T15:54:48+02:00
+draft: true
+---
+
+This is a calendar fact generator based on [xkcd#1930](https://xkcd.com/1930/).
+
+You can view the code in the [GIST](https://gist.github.com/gardient/2085f645706090426517c31cbaa9417d#file-xkcd-calendar-fact-generator-js-L176)
---
+++ static/js/xkcd-calendar-facts.js
@@ -0,0 +1,227 @@
+let generateXkcdCalendarFact = (function () { // eslint-disable-line no-unused-vars
+  let first = [
+    [
+      'the ',
+      [
+        'fall ',
+        'spring '
+      ],
+      'equinox '
+    ],
+    [
+      'the ',
+      [
+        'winter ',
+        'summer '
+      ],
+      [
+        'solistice ',
+        'olympics '
+      ]
+    ],
+    [
+      'the ',
+      [
+        'earliest ',
+        'latest '
+      ],
+      [
+        'sunrise ',
+        'sunset '
+      ]
+    ],
+    [
+      'daylight ',
+      [
+        'saving ',
+        'savings '
+      ],
+      'time '
+    ],
+    [
+      'leap ',
+      [
+        'day ',
+        'year '
+      ]
+    ],
+    'easter ',
+    [
+      'the ',
+      [
+        'harvest ',
+        'super ',
+        'blood '
+      ],
+      'moon '
+    ],
+    'Toyota truck month ',
+    'shark week '
+  ]
+
+  let second = [
+    [
+      'happens ',
+      [
+        'earlier ',
+        'later ',
+        'at the wrong time '
+      ],
+      'every year'
+    ],
+    [
+      'drifts out of sync with the ',
+      [
+        'sun ',
+        'moon ',
+        'zodiac ',
+        [
+          [
+            'gregorian ',
+            'mayan ',
+            'lunar ',
+            'iPhone '
+          ],
+          'calendar'
+        ],
+        'atomic clock in Colorado'
+      ]
+    ],
+    [
+      'might ',
+      [
+        'not happen ',
+        'happen twice '
+      ],
+      'this year'
+    ]
+  ]
+
+  let third = [
+    [
+      'time zone legislation in ',
+      [
+        'Indiana',
+        'Arizona',
+        'Russia'
+      ]
+    ],
+    'a decree by the Pope in the 1500s',
+    [
+      [
+        'precession ',
+        'libration ',
+        'nutation ',
+        'libation ',
+        'eccentricity ',
+        'obliquity'
+      ],
+      'of the ',
+      [
+        'moon',
+        'sun',
+        'earth\'s axis',
+        'equator',
+        'prime meridian',
+        [
+          [
+            'international date ',
+            'Mason-Dixon '
+          ],
+          'line'
+        ]
+      ]
+    ],
+    'magnetic field reversal',
+    [
+      'an arbitrary decision by ',
+      [
+        'Benjamin Franklin',
+        'Isaac Newton',
+        'FDR'
+      ]
+    ]
+  ]
+
+  let fourth = [
+    'it causes a predictable increase in car accidents.',
+    'that\'s why we have leap seconds.',
+    'scientists are really worried.',
+    [
+      'it was even more extreme during the ',
+      [
+        [
+          [
+            'bronze ',
+            'ice '
+          ],
+          'age.'
+        ],
+        'cretaceus.',
+        '1990s.'
+      ]
+    ],
+    [
+      'there is a proposal to fix it, but it ',
+      [
+        'will never happen.',
+        'actually makes things worse.',
+        'is stalled in congress.',
+        'might be unconstitutional.'
+      ]
+    ],
+    'it\'s getting worse and no one knows why.'
+  ]
+
+  let actualDataStructure = [ // pick one of each
+    'Did you know that <u>',
+    first,
+    second,
+    '</u> because of <u>',
+    third,
+    '</u>? Apparently <u>',
+    fourth,
+    '</u>'
+  ]
+
+  function getRandomItem (arr) {
+    if (!Array.isArray(arr)) throw new TypeError('This is not an array yo!')
+
+    return arr[(Math.floor(Math.random() * arr.length))]
+  }
+
+  function pickRandom (arr) {
+    let rnd = getRandomItem(arr)
+
+    if (Array.isArray(rnd)) return pickOneOfEach(rnd)
+    else return rnd
+  }
+
+  function pickOneOfEach (arr) {
+    let result = ''
+
+    for (let el of arr) {
+      if (Array.isArray(el)) {
+        result += pickRandom(el)
+      } else {
+        result += String(el)
+      }
+    }
+
+    return result
+  }
+
+  return function generateFact () {
+    return pickOneOfEach(actualDataStructure)
+  }
+})()
+
+let factDisplay = document.getElementById('xkcd-calendar-fact')
+let generateBtn = document.getElementById('generate-xkcd-calendar-fact')
+
+function generateAndDisplayFact () {
+  factDisplay.innerHTML = generateXkcdCalendarFact()
+}
+
+generateBtn.addEventListener('click', generateAndDisplayFact)
+generateAndDisplayFact()
