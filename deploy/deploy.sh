df41faccccd7cd4024bddcbb8f53cb3ce6839193
Gardient <gradient93@gmail.com>
2017-12-20 14:44:34 +0200

    updates deploy.sh

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,2 +1,2 @@
 rsync -r --delete-after --quiet -og --chown=www-data:www-data $TRAVIS_BUILD_DIR/public.tech/* www-deploy@gyulakerezsi.tech:/usr/share/nginx/gyulakerezsi.tech/site/
-rsync -r --delete-after --quiet -og --chown=www-data:www-data $TRAVIS_BUILD_DIR/public/* www-deploy@gyulakerezsi.tech:/usr/share/nginx/gyulakerezsi.ro/site/
+rsync -r --delete-after --quiet -og --chown=www-data:www-data $TRAVIS_BUILD_DIR/public/* www-deploy@gyulakerezsi.tech:/usr/share/nginx/gyulakerezsi.ro/
