3b9754167d1756a5d7c594cabe7d97e21c411396
Gardient <gradient93@gmail.com>
2018-02-18 19:44:14 +0200

    make deploy less quiet

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,1 +1,1 @@
-rsync -r --delete-after --quiet -og --chown=www-data:www-data $TRAVIS_BUILD_DIR/public/* www-deploy@gyulakerezsi.ro:/usr/share/nginx/gyulakerezsi.ro/
+rsync -r --delete-after -og --chown=www-data:www-data $TRAVIS_BUILD_DIR/public/* www-deploy@gyulakerezsi.ro:/usr/share/nginx/gyulakerezsi.ro/
